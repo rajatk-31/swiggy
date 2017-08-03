00e07956387d3f72cb941ec49879e2f7cb2cb579
surajsaluja4 <suraj.saluja.4@gmail.com>
2017-08-03 12:13:56 +0530

    restaurant data

---
+++ restaurant_data.js
@@ -0,0 +1,54 @@
+building:[{
+	Building_id:{
+	type:String,
+	required: 'Building Id Required'
+	},
+
+	Building_address :{
+	type:String,
+	required: 'Building address Required'
+	} 
+	Building_Name:{
+	type:String,
+	required: 'Building Name Required'
+	}
+
+	Restaurant:[{
+	Restaurant_id:{
+	type:String,
+	required: 'Building Id Required'
+	},
+
+	Restaurant_address :{
+	type:String,
+	required: 'Building address Required'
+	}, 
+	Restaurant_Name:{
+	type:String,
+	required: 'Building Name Required'
+	},
+	Restaurant_status:{
+	type:String,
+	required: 'Building Name Required'
+	}
+
+	Products:[{
+	Product_id:{
+	type:String,
+	required: 'Building Id Required'
+	},
+
+	Product_Name:{
+	type:String,
+	required: 'Building Name Required'
+	},
+	Product_status:{
+	type:String,
+	required: 'Building Name Required'
+	}
+
+	}]
+
+	}]
+
+}]
